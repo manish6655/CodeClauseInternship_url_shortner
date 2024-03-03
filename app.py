58082f9bb294905088666ee5da2f7e1e23ce84f4
unknown <manish66mr66@gmail.com>
2024-03-03 19:36:29 +0530

    files added

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template, redirect
+from utils import *
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        short_name = request.form.get('name')
+        url = request.form.get('url')
+
+        if 'http' not in url:
+            url = f'http://{url}'
+
+        if valid_url(url):
+            if name_available(short_name) is None:
+                add_url(short_name, url)
+                return render_template('success.html', short_name=short_name)
+            else:
+                return render_template('index.html', msg='Short name not available')
+        else:
+            return render_template('index.html', msg='Invalid url')
+        
+    return render_template('index.html')
+
+@app.route('/<path:short_name>')
+def redirect_url(short_name):
+    url = get_url(short_name)
+    if url is None:
+        return "<h2 style='color:red> Invalid URL </h2>"
+    return redirect(url.url)
---
+++ utils.py
@@ -0,0 +1,29 @@
+import validators
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import psycopg2
+
+DATABASE_URL = 'postgresql://postgres:root@localhost:6666/url_shortner'
+engine = create_engine(DATABASE_URL)
+db = scoped_session(sessionmaker(bind = engine))
+
+
+def valid_url(url):
+    return validators.url(url) is True
+
+def name_available(name):
+    return db.execute("select short_name from url_info where short_name = :name",
+    {"name" : name}).fetchone()
+
+def add_url(name, url):
+    db.execute("insert into url_info (short_name, url) values (:name, :url)",
+    {
+        "name": name,
+        "url" : url    
+    })
+    db.commit()
+
+def get_url(short_name):
+    return db.execute("select * from url_info where short_name = :short_name",
+    {"short_name" : short_name}).fetchone()
+    
